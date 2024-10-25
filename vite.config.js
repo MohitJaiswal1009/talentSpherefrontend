7b911f7aee1a7e3fda94d65040c6f40e4fa168f0
MohitJaiswal1009 <mohitjaiswal1009@gmail.com>
2024-10-25 13:12:46 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // Ensure the port number matches Render’s expected port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000, // Use the same port for preview
+  },
 })
